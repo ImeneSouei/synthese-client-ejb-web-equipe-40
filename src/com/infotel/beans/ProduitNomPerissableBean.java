6f1553559b5db0a92ca9b93961cc9718d041bbac
Soulaima MEZGHANI <soulayma.mezghani@gmail.com>
2019-05-23 16:07:27 +0200

    produit nom perissable et perissable

---
+++ src/com/infotel/beans/ProduitNomPerissableBean.java
@@ -0,0 +1,60 @@
+package com.infotel.beans;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import com.infotel.ejb.IProduitRemote;
+import com.infotel.metier.ProduitNonPerissable;
+
+@Named
+@SessionScoped
+public class ProduitNomPerissableBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	// Attributs
+			@EJB
+			private IProduitRemote service;
+			public IProduitRemote getService() {
+				return service;
+			}
+			public void setService(IProduitRemote service) {
+				this.service = service;
+			}
+			public static long getSerialversionuid() {
+				return serialVersionUID;
+			}
+			private ProduitNonPerissable produitnomperissable= new ProduitNonPerissable();
+			public ProduitNonPerissable getProduitnomperissable() {
+				return produitnomperissable;
+			}
+			public void setProduitnomperissable(ProduitNonPerissable produitnomperissable) {
+				this.produitnomperissable = produitnomperissable;
+			}
+			// Méthodes
+			
+			
+			public void ajouterProduitNonPerissable() {
+				service.ajouterProduitNonPerissable(produitnomperissable);
+				produitnomperissable = new ProduitNonPerissable();
+			}
+			public List<ProduitNonPerissable>  findAllProduitNonPerissable(){
+				return service. findAllProduitNonPerissable();
+			}
+			public void modifierProduitNonPerissable() {
+				service.modifierProduitNonPerissable(produitnomperissable);
+				produitnomperissable = new ProduitNonPerissable();
+			}
+			public void supprimerProduitNonPerissable(long idProduit) {
+				service.supprimerProduitNonPerissable(idProduit);
+			}
+			public ProduitNonPerissable getProduitNonPerissable(long idProduit) {
+				return service.getProduitNonPerissable(idProduit);
+			}
+			public ProduitNonPerissable affichageProduitNonPerissable(long idProduit) {
+				return service.affichageProduitNonPerissable(idProduit);
+			}
+			
+}
---
+++ src/com/infotel/beans/ProduitPerissableBean.java
@@ -0,0 +1,62 @@
+package com.infotel.beans;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import com.infotel.ejb.IProduitRemote;
+import com.infotel.metier.ProduitNonPerissable;
+import com.infotel.metier.ProduitPerissable;
+@Named
+@SessionScoped
+public class ProduitPerissableBean implements Serializable{
+	@EJB
+	private IProduitRemote service;
+
+	public IProduitRemote getService() {
+		return service;
+	}
+
+	public void setService(IProduitRemote service) {
+		this.service = service;
+	}
+	private ProduitPerissable produitperissable= new ProduitPerissable();
+
+	public ProduitPerissable getProduitperissable() {
+		return produitperissable;
+	}
+
+	public void setProduitperissable(ProduitPerissable produitperissable) {
+		this.produitperissable = produitperissable;
+	}
+	// Méthodes
+	
+	
+				public void ajouterProduitPerissable() {
+					service.ajouterProduitPerissable(produitperissable);
+					produitperissable = new ProduitPerissable();
+				}
+				public List<ProduitNonPerissable>  findAllProduitNonPerissable(){
+					return service. findAllProduitNonPerissable();
+				}
+				public void modifierProduitPerissable() {
+					service.modifierProduitPerissable(produitperissable);
+					produitperissable = new ProduitPerissable();
+				}
+				public void supprimerProduitPerissable(long idProduit) {
+					service.supprimerProduitNonPerissable(idProduit);
+				}
+				public ProduitPerissable getProduitPerissable(long idProduit) {
+					return service.getProduitPerissable(idProduit);
+				}
+				public ProduitPerissable affichageProduitPerissable(long idProduit) {
+					return service.affichageProduitPerissable(idProduit);
+				}
+				
+	}
+
+
+
